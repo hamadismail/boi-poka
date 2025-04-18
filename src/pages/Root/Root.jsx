e07a59c793376799be0f758073c04699e2cf63be
hamadismail <hamad.ismail.gub@gmail.com>
2025-04-18 18:21:58 +0600

    implement-react-spinner

--- src/pages/Root/Root.jsx
+++ src/pages/Root/Root.jsx
@@ -1,14 +1,25 @@
 import React from 'react';
 import Navbar from '../../components/Header/Navbar';
-import { Outlet } from 'react-router';
+import { Outlet, useNavigation } from 'react-router';
 import Footer from '../../components/Footer/Footer';
 import './root.css';
+import { PulseLoader } from 'react-spinners';
 
 const Root = () => {
+  const navigation = useNavigation();
+  const isNavigating = Boolean(navigation.location);
   return (
     <div>
       <Navbar></Navbar>
-      <Outlet></Outlet>
+
+      {isNavigating ? (
+        <div className="flex justify-center mt-8">
+          <PulseLoader color="#2f4f4f"></PulseLoader>
+        </div>
+      ) : (
+        <Outlet></Outlet>
+      )}
+
       <Footer></Footer>
     </div>
   );
